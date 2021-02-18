b3e3e7391e89307e8b714a6aad6a848d208a5519
Syed Ameen Quadri <ameenquadri2000@gmail.com>
2021-02-18 17:00:45 -0500

    Add files via upload

---
+++ lab06.X/build/default/debug/labF.o.d
@@ -0,0 +1,2 @@
+build/default/debug/labF.o:  \
+labF.cpp 
---
+++ lab08.X/build/default/debug/assembler_for_main.o.d
@@ -0,0 +1,8 @@
+build/default/debug/assembler_for_main.o:  \
+assembler_for_main.S 
+build/default/debug/assembler_for_main.o:  \
+ C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.35/packs/Microchip/PIC32MX_DFP/1.2.228/include/proc/p32mx150f128b.h  \
+ C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.35/packs/Microchip/PIC32MX_DFP/1.2.228/include/xc-pic32m.h  \
+ C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.35/packs/Microchip/PIC32MX_DFP/1.2.228/include/xc.h  \
+  \
+assembler_for_main.S  \
